efde546fea73dacca85c74ba57ff65a968402ce3
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-09 14:04:46 -0300

    feat: added user registration form

--- TrainSys/src/views/register/User/User.vue
+++ TrainSys/src/views/register/User/User.vue
@@ -1,3 +1,68 @@
 <template>
-    <h1>Página Cadastro de usuário</h1>
+  <h1>Crie sua conta</h1>
+    <v-form>
+      <v-text-field
+      label="Nome completo" 
+      placeholder="Insira o nome completo" 
+      type="text"
+      v-model="name"
+      variant="outlined" 
+      />
+  
+      <v-text-field
+      label="Email" 
+      placeholder="Insira o email" 
+      type="email"
+      v-model="email"
+      variant="outlined"
+      />
+  
+      <v-text-field
+      label="Senha" 
+      placeholder="Insira a senha" 
+      type="password"
+      v-model="password"
+      variant="outlined"
+      />
+  
+      <v-text-field
+      label="Confirme a senha" 
+      placeholder="Insira a senha novamente" 
+      type="password"
+      v-model="passwordConfirmation"
+      variant="outlined"
+      />
+  
+      <v-select
+      label="Qual o plano?" 
+      placeholder="Escolha o plano que deseja" 
+      :items="plans" 
+      item-title="plan" 
+      item-value="value" 
+      type="text"
+      variant="outlined"
+      />
+          
+      <v-btn type="submit">Cadastrar</v-btn>
+    </v-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      type_plan: "bronze",
+      plans:
+      [
+        {plan: 'Bronze', value: 'bronze'},
+        {plan: 'Prata', value: 'silver'},
+        {plan: 'Ouro', value: 'gold'},
+      ],
+    }
+  }
+}
+</script>
