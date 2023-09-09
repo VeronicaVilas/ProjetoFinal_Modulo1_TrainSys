cc4a2d2a9e4b817f511dd0090296d8a594d72281
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-09 19:13:02 -0300

    feat: added user registration form

--- TrainSys/src/views/login/Login.vue
+++ TrainSys/src/views/login/Login.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>Página Login</h1>
+  <h1>Login</h1>
+
+  <v-form @submit.prevent="handleSubmitUser">
+    <v-text-field
+    label="Email" 
+    placeholder="Insira o email" 
+    type="email"
+    v-model="email"
+    variant="outlined"
+    />
+
+    <v-text-field
+    label="Senha"
+    placeholder="Insira a senha"
+    v-model="password"
+    :type="passwordVisibility ? 'text' : 'password'"
+    variant="outlined"
+    :append-inner-icon="passwordVisibility ? 'mdi-eye-off' : 'mdi-eye'"
+    @click:append-inner="passwordVisibility = !passwordVisibility"
+    />    
+
+    <v-btn class="button-form" size="large" type="submit">Entrar</v-btn>
+  </v-form>
+
+  <p class="mb-2">Ainda não tem conta? 
+    <router-link to="/cadastro/usuario" class="register"> Cadastre-se</router-link>
+  </p>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      passwordVisibility: false,
+    }
+  }
+}
+</script>
