3bc2f75671793953c1c4d40f0aaef8fd1405a8bc
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-09 19:58:48 -0300

    feat: Added v-card to display the number of registered students and the number of exercises.

--- TrainSys/src/views/dashboard/Dashboard.vue
+++ TrainSys/src/views/dashboard/Dashboard.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Página Dashboard</h1>
+  <h1 class="title d-flex mt-8 ml-10">Bem vindo(a)</h1>
+
+  <v-card variant="outlined">
+    <v-card-text class="d-flex">
+      <p>Lista de alunos</p>
+      <p>Alunos cadastrados</p>
+      
+      <router-link to="/gerenciamento/alunos">
+        <v-btn size="large" type="submit">Vizualizar</v-btn>
+      </router-link>
+    </v-card-text>
+  </v-card>
+  
+  <v-card variant="outlined">
+    <v-card-text class="d-flex">
+      <p>Cadastro de exercícios</p>
+      <p>Exercicios cadastrados</p>
+      
+      <router-link to="/gerenciamento/exercicios">
+        <v-btn size="large" type="submit">Adicionar</v-btn>
+      </router-link>
+
+    </v-card-text>
+  </v-card>
 </template>
