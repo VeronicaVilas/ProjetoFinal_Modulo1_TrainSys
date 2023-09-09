e2cd0b8cd4182001831c65d373a853c3d94e5ea9
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-09 12:11:53 -0300

    build: added vuetify npm dependency

--- TrainSys/src/main.js
+++ TrainSys/src/main.js
@@ -3,4 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Configurações do Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
