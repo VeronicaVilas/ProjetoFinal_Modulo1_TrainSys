f66e2679e5c20f42201f15e97d24b9762d6bd26e
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-10 20:23:02 -0300

    style: added styling to the page and inserted an input disabled for display

--- TrainSys/src/views/register/training/Training.vue
+++ TrainSys/src/views/register/training/Training.vue
@@ -1,27 +1,51 @@
 <template>
-    <h1>Treino</h1>
-    <p>Matrícula: {{ this.$route.params.id }}</p>
-    <p>Nome: {{ this.$route.query.title }}</p>
+
+  <div class="d-flex">
+    <v-icon class="icon mt-6 ms-5" icon="mdi-dumbbell" size="x-large"></v-icon>
+    <h1 class="title mt-10 ml-2">Treino</h1>
+  </div>
+
+  <hr>
   
-    <v-form @submit.prevent="handleSubmitTraining">
-      <v-select
-      label="Qual o exercício?" 
-      placeholder="Escolha o exercicio" 
-      :items="exercises" 
-      item-title="description"
-      item-value="id"
-      type="text"
-      v-model="exercise_id"
-      class="w-25 px-2" variant="outlined"
+  <v-form class="d-flex flex-wrap" disabled>
+      <v-text-field
+      v-model="student_id"
+      label="Matrícula"
+      variant="outlined"
+      class="px-2 mt-5"
       />
-  
+      <v-text-field
+      v-model="student_name"
+      label="Nome"
+      variant="outlined"
+      class="px-2 mt-5"
+      />
+    </v-form>
+
+  <v-form @submit.prevent="handleSubmitTraining">
+
+    <v-select
+    label="Qual o exercício?" 
+    placeholder="Escolha o exercicio" 
+    :items="exercises" 
+    item-title="description"
+    item-value="id"
+    type="text"
+    v-model="exercise_id"
+    variant="outlined"
+    class="px-2 mb-3"
+    />
+
+    <div class="d-flex flex-wrap">
+
       <v-text-field
       label="Repetições" 
       placeholder="Insira o número de repetições" 
       type="number"
       v-model="repetitions"
       :error-messages="this.errorValidation.repetitions"
-      class="w-25 px-2" variant="outlined" 
+      variant="outlined"
+      class="w-30 px-2 mb-3" 
       />
   
       <v-text-field
@@ -31,18 +55,20 @@
       v-model="weight"
       suffix="kg"
       :error-messages="this.errorValidation.weight"
-      class="w-25 px-2" variant="outlined" 
+      variant="outlined" 
+      class="w-30 px-2 mb-3"
       />
-  
+
       <v-text-field
       label="Tempo de pausa" 
       placeholder="Insira o tempo da pausa" 
       type="time"
       v-model="break_time"
       :error-messages="this.errorValidation.break_time"
-      class="w-25 px-2" variant="outlined" 
+      variant="outlined" 
+      class="w-30 px-2 mb-3"
       />
-  
+
       <v-select
       label="Dia da semana" 
       :items="week" 
@@ -50,19 +76,24 @@
       item-value="value" 
       type="text"
       v-model="day"
-      class="w-25 px-2" variant="outlined"
+      variant="outlined"
+      class="w-30 px-2 mb-3"
       />
+    </div>
   
-      <v-text-field
-      label="" 
-      placeholder="Observações para o treino" 
-      type="text"
-      v-model="observations"
-      class="w-25 px-2" variant="outlined" 
-      />
+    <v-text-field
+    label="" 
+    placeholder="Observações para o treino" 
+    type="text"
+    v-model="observations"
+    variant="outlined" 
+    class="input-observation px-2"
+    />
   
-      <v-btn color="success" class="mt-1 me-2" size="large" type="submit">Cadastrar</v-btn>
-    </v-form>
+    <div class="d-flex justify-center">
+      <v-btn class="button d-flex ml-5" size="x-large" type="submit">Cadastrar</v-btn>
+    </div>
+  </v-form>
 </template>
 
 <script>
@@ -74,6 +105,7 @@ export default {
   data() {
     return {
       student_id: this.$route.params.id,
+      student_name: this.$route.query.title,
       repetitions: "",
       weight: "",
       break_time: "",
@@ -132,6 +164,34 @@ export default {
           { abortEarly:false }
         )
 
+        axios ({
+          url: 'http://localhost:3000/workouts',
+          method: 'POST',
+          data: {
+            student_id: this.student_id,
+            exercise_id: this.exercise_id,
+            repetitions: this.repetitions,
+            weight: this.weight,
+            break_time: this.break_time,
+            day: this.day,
+            observations: this.observations
+          }
+          })
+          .then(() => {
+            alert('Treino cadastrado com sucesso!')
+            
+            this.student_id = ''
+            this.exercise_id = ''
+            this.repetitions = ''
+            this.weight = ''
+            this.break_time = ''
+            this.day = ''
+            this.observations = ''
+
+          })
+          .catch(() => {
+            alert('Não foi possível cadastrar um treino nesse momento, por favor, tente novamente mais tarde.')})
+
       } catch (error) {
           if (error instanceof yup.ValidationError) {
             console.log(error)
@@ -146,12 +206,53 @@ export default {
     axios ({
       url: 'http://localhost:3000/exercises',
       method: 'GET',
-      })
-      .then((response) => {
-        this.exercises = response.data
-      })
-      .catch(() => {
-        alert('Não foi possível carregar a lista de exercícios nesse momento, por favor, tente novamente mais tarde.')})
-    }
+    })
+    .then((response) => {
+      this.exercises = response.data
+    })
+    .catch(() => {
+      alert('Não foi possível carregar a lista de exercícios cadastrados nesse momento, por favor, tente novamente mais tarde.')})
+  }
 }
 </script>
+
+<style scoped>
+  .title {
+    text-transform: uppercase;
+    font-size: 32px;
+  }
+  .icon {
+    font-size: 60px;
+  }
+
+  hr{
+    border: 1px solid;
+  }
+
+  .name-student-subtitle {
+    font-size: 18px;
+    padding: 8px;
+    background: linear-gradient(80deg, #1976D2, #1337B4);
+  }
+  .input-observation {
+    height: 200px;
+  }
+
+  .button {
+    margin-top: -60px;
+    width: 300px;
+
+    text-transform: lowercase;
+    font-weight: bold;
+    font-size: 18px;
+
+    background: linear-gradient(80deg, #1976D2, #1337B4);
+    cursor: pointer;
+  }
+
+  .button:hover {
+    background: black;
+    color: white;
+    box-shadow: 0 0 30px rgba(19, 55, 180, .5);
+  }
+</style>
