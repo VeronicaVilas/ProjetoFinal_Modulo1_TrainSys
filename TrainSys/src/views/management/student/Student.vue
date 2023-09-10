7491174aa2442e1b4aab4304ccefbaa7edc91e8f
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-10 13:40:05 -0300

    feat: search bar and table for student display

--- TrainSys/src/views/management/student/Student.vue
+++ TrainSys/src/views/management/student/Student.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>Página Gerenciamento de alunos</h1>
+  <h1>Alunos</h1>
+  <router-link to="/cadastro/novo-aluno">
+    <v-btn prepend-icon="mdi-account-plus" type="submit">Novo</v-btn>
+  </router-link>
+
+  <hr>
+
+  <v-form>
+    <v-text-field
+    label=""
+    placeholder="Insira o nome do aluno para pesquisar"
+    type="text"
+    v-model="name"
+    variant="outlined"
+    />
+
+    <v-btn size="x-large" prepend-icon="mdi-account-search" type="submit">Buscar</v-btn>
+  </v-form>
+
+  <v-table>
+    <thead>
+      <tr>
+        <th class="subtitle-table">Matrícula</th>
+        <th class="subtitle-table">Nome</th>
+        <th class="subtitle-table">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>
+          <v-btn title="Clique para moontar um treino" prepend-icon="mdi-dumbbell" type="submit" >
+          Treinos
+          </v-btn>
+
+          <v-btn title="Vizualizar treino" variant="text" icon="mdi-eye-outline"></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    }
+  }
+}
+</script>
