add0b3d06949507110fd80c56180e89990c65e2a
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-09-10 11:50:21 -0300

    feat: added input and table for registration and display, respectively

--- TrainSys/src/views/management/exercise/Exercise.vue
+++ TrainSys/src/views/management/exercise/Exercise.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Página  Gerenciamento de exercicios</h1>
+  <v-form>
+    <v-text-field
+    label="" 
+    placeholder="Insira o nome do exercício para cadastro" 
+    type="text"
+    v-model="description"
+    variant="outlined"  
+    />
+  
+    <v-btn>Cadastrar</v-btn>
+  </v-form>
+  
+  <v-table >
+    <thead>
+      <tr>
+        <th>Nome</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="exercises in exerciseList" :key="exercises.id"> 
+        <td>{{ exercises.description }}</td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      description: "",
+      exerciseList: [],
+    }
+  }
+}
+</script>
+  
